9783b746c2d19aaef285f216b095fc79a9c428ac
ChineduOscar <chinedunnadoziepatrick@gmail.com>
2024-12-26 20:35:23 -0800

    added api/v1 to the backend domain

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const API_URL = 'https://techvanapi.onrender.com'
+export const API_URL = 'https://techvanapi.onrender.com/api/v1'
 
 export default function init() {
   axios.defaults.baseURL = API_URL
