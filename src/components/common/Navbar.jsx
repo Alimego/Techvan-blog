4549adb16e969dc7afa5166419fac23cc1fecf10
ChineduOscar <chinedunnadoziepatrick@gmail.com>
2024-12-28 05:17:34 -0800

    fixed responsiveness in navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -14,11 +14,11 @@ const Navbar = () => {
     <div className='bg-white fixed shadow-sm py-2 md:py-2 px-4 md:px-16 w-full flex items-center justify-between'>
       <Link to='/'>
         <div className='flex items-center gap-2 justify-center' onClick={toTop}>
-          <img src={techvan} alt='TECHVAN' className='w-[20px] md:w-[40px] h-[20px] md:h-[40px]'/>
+          <img src={techvan} alt='TECHVAN' className='w-[20px] md:w-[40px] h-[25px] md:h-[40px]'/>
           <p className='block text-[18px] md:text-2xl font-bold text-primary font-[Poppins]'>TECHVAN</p>
         </div>
       </Link>
-      <div className='flex items-center gap-4 md:gap-10'>
+      <div className='flex items-center gap-6 md:gap-12'>
         <div className='cursor-pointer'>
           <Link to='/search'>
             <SearchIcon />
